33b002a56d5a2cba4017c0116bbdd6a784b6c801
sleekmason <sleekmason@gmail.com>
2022-02-24 16:45:31 -0600

    Added a welcome screen for first run live:)

---
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Welcome script for Lilidog made by sleekmason 2-24-22
+
+full_fs=$(df ~ | tail -1 | awk '{print $1;}')  # find partition
+fs=$(basename "$full_fs")
+if grep -q "$fs" /proc/partitions; then
+   sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+else
+  yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
+--width=540 --height=360 --center --escape-ok --center --undecorated \
+--skip-taskbar --no-buttons --close-on-unfocus \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 10" \
+--fore="#3DCCC2"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
+fi
