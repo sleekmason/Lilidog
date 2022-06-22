f3ccefa5de15054f5216da4250f9ddb01653a469
sleekmason <sleekmason@gmail.com>
2022-06-22 15:09:41 -0500

    Adjusted welcome dialog to include paw directions.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -7,7 +7,7 @@ if grep -q "$fs" /proc/partitions; then
    sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
---width=488 --height=270 --center --escape-ok --undecorated \
+--width=488 --height=340 --center --escape-ok --undecorated \
 --skip-taskbar  --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
 --fore="#3DCCC2"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
