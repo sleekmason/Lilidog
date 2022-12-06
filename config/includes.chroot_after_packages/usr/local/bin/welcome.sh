e338c337080eef368abb056755a6de7661c77384
sleekmason <sleekmason@gmail.com>
2022-12-06 13:09:20 -0600

    Added ld-entry script for initial lilidog customizations.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -4,11 +4,16 @@
 full_fs=$(df ~ | tail -1 | awk '{print $1;}')  # find partition
 fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
-   sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+  yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
+--width=488 --height=444 --center --escape-ok --undecorated --skip-taskbar \
+--button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"xfce4-terminal --geometry=68x20-80-80 -T 'Customization' -e 'sudo ld-entry -i'" \
+--button="gtk-ok:0" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
+--fore="#94C6F7"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
---width=488 --height=340 --center --escape-ok --undecorated \
---skip-taskbar  --button="gtk-ok:0" \
---text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
+--width=488 --height=340 --center --escape-ok --undecorated --skip-taskbar \
+--button="gtk-ok:0" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Light 11" \
 --fore="#94C6F7"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
