3cb200d18b51786f54ad3c403151c58ddc0318e4
sleekmason <sleekmason@gmail.com>
2022-12-15 08:31:24 -0600

    Full theme change to Cube.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -9,11 +9,11 @@ if grep -q "$fs" /proc/partitions; then
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"xfce4-terminal --geometry=68x20-80-80 -T 'Customization' -e 'sudo ld-entry -i'" \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
---fore="#94C6F7"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+--fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=340 --center --escape-ok --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Light 11" \
---fore="#94C6F7"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
+--fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
