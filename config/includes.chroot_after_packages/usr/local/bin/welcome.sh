c037e8033a3e6fd25a57f7fce44c99ebf33424f2
sleekmason <sleekmason@gmail.com>
2023-02-13 10:57:27 -0600

    Camares installer adjustments. - Geometry for apt update and the entry script changed to default, and to x-terminal-emulator from xfce4-terminal.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -6,7 +6,7 @@ fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=444 --center --escape-ok --undecorated --skip-taskbar \
---button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"xfce4-terminal --geometry=68x20-80-80 -T 'Customization' -e 'sudo ld-entry -i'" \
+--button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo ld-entry -i'" \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
 --fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
