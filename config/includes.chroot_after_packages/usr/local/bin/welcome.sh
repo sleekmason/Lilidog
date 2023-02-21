3ab0b916a520c53b9cd1133ff18833eaaf283f7e
sleekmason <sleekmason@gmail.com>
2023-02-21 15:46:33 -0600

    adjusted Welcome text color.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -9,11 +9,11 @@ if grep -q "$fs" /proc/partitions; then
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo ld-entry -i'" \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 11" \
---fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+--fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=488 --height=340 --center --escape-ok --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Light 11" \
---fore="#DBDBDB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
+--fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
 fi
